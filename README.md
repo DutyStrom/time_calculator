ba19066b5f60e6b0db30e1265e3419d0ed86b7dc
DutyStrom <petr.bocek13@gmail.com>
2025-09-06 10:59:08 +0200

    Commit main file
    Create and commit new main file with header and function declaration
    
     * add: main file "time_calculator.py"

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # time_calculator
+
 Add duration time to the start time and return result
---
+++ time_calculator.py
@@ -0,0 +1,14 @@
+# Time Calculator
+# second Python project from freecodecamp.org website
+
+# author: Petr Boček
+# mail: Bocek2@seznam.cz
+# Discord: Seth_Cz#8510
+# GitHub: https://github.com/DutyStrom
+
+# repo: https://github.com/DutyStrom/time_calculator.git
+
+# -----------------------------------------------------------
+
+def add_time(start, duration, day):
+    pass
